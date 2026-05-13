ba14434aabf6627a420f9b2597814c9fdacdaf1e
HangHo <hang3ho@gmail.com>
2026-05-13 16:23:10 +0700

    restructure pj

--- src/controllers/web/auth.controller.js
+++
@@ -1,3 +0,0 @@
-exports.showLogin = async (req, res) => {
-  res.render('pages/login', { layout: false, data: {} });
-};
--- src/controllers/web/stat.controller.js
+++
@@ -1,7 +0,0 @@
-exports.showStatByMonth = async (req, res) => {
-  res.render('pages/stats/monthly', { layout: 'layouts/main', activeMenu: 'statsMonth', data: {} });
-};
-
-exports.showStatByYear = async (req, res) => {
-  res.render('pages/stats/yearly', { layout: 'layouts/main', activeMenu: 'statsYear', data: {} });
-};
